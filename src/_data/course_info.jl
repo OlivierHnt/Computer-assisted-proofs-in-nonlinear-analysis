068e13b5e0930d83e38a5950ac215d6bbcc894a7
OlivierHnt <38465572+OlivierHnt@users.noreply.github.com>
2024-07-10 18:18:00 +0200

    Update course info

--- src/_data/course_info.jl
+++ src/_data/course_info.jl
@@ -2,9 +2,13 @@ Dict(
     "course_name" => "Computer-assisted proofs in nonlinear analysis",
     "course_subtitle" => "",
     "code" => "",
-    "semester" => "Fall 2024",
+    "semester" => "September 2024",
     "authors" => [
-        "Olivier Hénot" => "https://olivierhnt.github.io/"
+        "Jan Bouwe van den Berg" => "https://math.vu.nl/~janbouwe/",
+        "Maxime Breden" => "https://sites.google.com/site/maximebreden/",
+        "Olivier Hénot" => "https://olivierhnt.github.io/",
+        "Jean-Philippe Lessard" => "https://www.math.mcgill.ca/jplessard/Home.html",
+        "Jason D. Mireles James" => "http://cosweb1.fau.edu/~jmirelesjames/index.html"
     ],
     "institution" => "RadiiPolynomial",
     "institution_logo" => "logo.png",
