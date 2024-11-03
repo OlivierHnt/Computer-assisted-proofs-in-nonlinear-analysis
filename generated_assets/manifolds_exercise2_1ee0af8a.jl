dde5441740c99ccd60d1bbe7206ae7a058b872ce
OlivierHnt <38465572+OlivierHnt@users.noreply.github.com>
2024-11-03 08:26:30 +0000

    Deploying to gh-pages from @ OlivierHnt/Computer-assisted-proofs-in-nonlinear-analysis@108902cd07f4b2c68a071ad29108925c637eccfb 🚀

---
+++ generated_assets/manifolds_exercise2_1ee0af8a.jl
@@ -0,0 +1,33 @@
+### A Pluto.jl notebook ###
+# v0.19.47
+
+#> [frontmatter]
+#> homework_number = 2
+#> order = 2
+#> title = "2D manifold"
+#> tags = ["module5", "homeworks"]
+#> layout = "layout.jlhtml"
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ a3982eb8-7843-11ef-0e0b-6be567b62442
+html"""
+<style>
+main {
+    max-width: 1000px;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: justify;
+}
+</style>
+"""
+
+# ╔═╡ 7e454145-e9b8-4348-85d7-56b79edea6ea
+md"""
+Compute the 2D stable manifold of the origin for Lorenz.
+"""
+
+# ╔═╡ Cell order:
+# ╟─a3982eb8-7843-11ef-0e0b-6be567b62442
+# ╟─7e454145-e9b8-4348-85d7-56b79edea6ea
